8fb3edb0fe40a7bca9b06818f6a601d17f7da6cb
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-18 22:52:58 -0500

    learn props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import LearnJSX from "./basicStuff/learnJSX";
+import Navbar from './basicStuff/LearnProps'
 
 function App() {
+
   return (
     <div className="App">
-      <LearnJSX/>
+      <Navbar title = "Github" subtitle ="test"/>
+      {/* <LearnJSX/> */}
     </div>
   );
 }
