42c691d549f33bb8ea2aff9f65caa8e03670dcc0
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-19 17:59:02 -0500

    Refactoring component

--- src/basicStuff/Users.js
+++ src/basicStuff/Users.js
@@ -1,19 +1,18 @@
 import { render } from '@testing-library/react'
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
 
-class Users extends Component {
+const Users=({users, loading})=> {
     
-   render(){
         return (
             <div style={userStyle}>
-                {this.props.users.map(user =>(
+                {users.map(user =>(
                     <UserItem key = {user.id} user = {user}/>
                 ))}
             </div>
         )
     }
-}
+
 
 const userStyle ={
     display: 'grid',
