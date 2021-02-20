5b5138d2d9ff2aca326b8c401ffcd53aa96c656a
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-20 11:42:21 -0500

    propTypes added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render(){
   return (
     <div className="App">
-     <Navbar/>
+     <Navbar title = 'Github Finder' icon = 'fab fa-github'/>
     </div>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import propTypes from 'prop-types'
 
 export class Navbar extends Component {
     static deafultProps = {
         title: 'Github Finder',
         icon: 'fab fa-github'
     }
+    static propTypes ={
+        title : propTypes.string.isRequired
+    }
+
     render() {
         return (
             <nav className='navbar bg-primary'>
