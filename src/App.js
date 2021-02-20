2f20353c83b0d725ba1ba93ebe628329d53e1162
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-20 11:25:16 -0500

    add deafult props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-// import LearnJSX from "./basicStuff/learnJSX";
+import './App.css'
 import React, {Component} from 'react'
-import Navbar from './basicStuff/LearnProps'
-import PropsFC from "./basicStuff/PropsFC";
-import Users from './basicStuff/Users'
-import axios from 'axios'
-
-
+import Navbar from './components/layout/Navbar'
 
 export default class App extends Component {
-  state = {
-    users: [],
-    loading: false
-  }
-  async componentDidMount(){
-    this.setState({loading: true});
-    const res = await axios.get('https://jsonplaceholder.typicode.com/photos')
-    this.setState({users: res.data, loading: false})
-
-  }
   render(){
   return (
     <div className="App">
-      <Navbar title = "Github" subtitle ="test"/> 
-      <PropsFC title = {2 +2} name = "Musa" />
-      <Users loading={this.state.loading} users = {this.state.users}/>
+     <Navbar/>
     </div>
   );
 }
--- src/basicStuff/PropsFC.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function props_fc(props) {
-    return (
-        <div>
-           <h1>Hello {props.title} {props.name}</h1> 
-        </div>
-    )
-}
