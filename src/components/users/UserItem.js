c69a803eeeea1366d98d45508a25ca02bc0b1070
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-24 11:53:14 -0500

    added users array hard coded

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
 
-export class UserItem extends Component {
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url:"https://github.com/mojombo"
-    }
+class UserItem extends Component {
 
     render() {
-        const {login, avatar_url, html_url}= this.state
+        const {login, avatar_url, html_url}= this.props.user
         return (
             <div className='card text-center'>
                 <img
@@ -17,9 +11,9 @@ export class UserItem extends Component {
                 alt=""
                 className='round-img'
                 style ={{width:'60px'}}/>
-                <h3>{this.state.login}</h3>
+                <h3>{this.login}</h3>
                 <div>
-                    <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">More</a>
+                    <a href={this.html_url} className="btn btn-dark btn-sm my-1">More</a>
                 </div>
             </div>
         )
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import UserItem from './UserItem'
 
-export default function Users() {
-    return (
-        <div>
-            <h1>From user</h1>
-        </div>
-    )
+class Users extends Component {
+    state={
+        users : [
+            {
+                id: 1,
+                login: "mojombo",
+                avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+                html_url: "https://github.com/mojombo"
+            },
+            {
+                id: 2,
+                login: "defunkt",
+                avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
+                html_url: "https://github.com/defunkt"
+            },
+            {
+                id: 3,
+                login: "pjhyett",
+                avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
+                html_url: "https://github.com/pjhyett"
+            }
+        ]
+    }
+    render() {
+        return (
+            <div>
+              {this.state.users.map(user=>(
+                  <UserItem key = {user.id} user = {user}/>
+              ))}  
+            </div>
+        )
+    }
 }
+
+export default Users
