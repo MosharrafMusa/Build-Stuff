a8015c186b69e2d68f7af37238a0e903f943f448
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-16 12:22:17 -0400

    wds lession04

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@
 import React, {Component} from 'react'
 
 import Lession01 from './components/webDevSimplified/Lession01'
+import Lession04 from './components/webDevSimplified/Lession04'
 
 export default class App extends Component {
   
   render(){
    
   return (
+    <>
     <div className="App">
     <Lession01/>
+    <Lession04/>
     </div>
+    </>
   );
 }
 }
---
+++ src/components/webDevSimplified/Lession04.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Lession04() {
+    return (
+        <div>
+            <button>-</button>
+            <span>count: 0</span>
+            <button>+</button>
+        </div>
+    )
+}
+
+export default Lession04
+
