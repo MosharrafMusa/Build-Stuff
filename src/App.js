bf3f56e77165cc3aff5910a6fc16becb293699f5
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-17 12:32:10 -0400

    wds-006

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react'
 import CounterClass from './components/webDevSimplified/CounterClass'
 import CounterClass2 from './components/webDevSimplified/CounterClass2';
+import CounterFunctional from './components/webDevSimplified/CounterFunctional';
 
 import Lession01 from './components/webDevSimplified/Lession01'
 import Lession04 from './components/webDevSimplified/Lession04'
@@ -13,7 +14,10 @@ export default class App extends Component {
   return (
     <>
     <div className="App">
+      Counter with Class Component
     <CounterClass2 initialCount = {0}/>
+    Counter with Hooks
+    <CounterFunctional initialCount={0}/>
     </div>
     </>
   );
---
+++ src/components/webDevSimplified/CounterFunctional.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+function CounterFunctional({initialCount}) {
+    const [count, setCount] = useState(initialCount)
+
+    return (
+        <div>
+            <button onClick={()=>setCount
+            (prevCount=>prevCount -1)}>-</button>
+            <span>{count}</span>
+            <button>+</button>
+        </div>
+    )
+}
+
+export default CounterFunctional
